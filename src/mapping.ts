14ce4d64457511851c5c20549a1f2d52654d62e4
Mattias Lightstone <mattias.lightstone@gmail.com>
2022-04-04 16:51:03 -0700

    Create a quick subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,3 +1,31 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { VotingVault } from "../generated/VotingVault/VotingVault"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt } from "@graphprotocol/graph-ts";
+import { VoteChange } from "../generated/VotingVault/VotingVault";
+import { VoteChangeEvent, Voter } from "../generated/schema";
+
+export function handleVoteChange(event: VoteChange): void {
+  const id =
+    event.logIndex.toString() + "-" + event.transaction.hash.toHexString();
+
+  const eventObject = new VoteChangeEvent(id);
+
+  eventObject.from = ensureVoter(event.params.from.toHexString()).id;
+
+  const toVoter = ensureVoter(event.params.to.toHexString());
+  toVoter.votingPower = toVoter.votingPower.plus(event.params.amount);
+  toVoter.save();
+  eventObject.to = toVoter.id;
+
+  eventObject.amount = event.params.amount;
+  eventObject.save();
+}
+
+function ensureVoter(address: string): Voter {
+  let voter = Voter.load(address);
+
+  if (!voter) {
+    voter = new Voter(address);
+    voter.votingPower = BigInt.zero();
+    voter.save();
+  }
+  return voter;
+}
