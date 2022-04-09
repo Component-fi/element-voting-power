47923c87d657ee7979b16ca7cdaecd4b54207106
Mattias Lightstone <mattias.lightstone@gmail.com>
2022-04-08 20:00:19 -0700

    Vesting vault reverse event args

---
+++ src/frozen_mapping.ts
@@ -0,0 +1,56 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import { VoteChange as FrozenVoteChange } from "../generated/FrozenLockingVault/FrozenLockingVault";
+import { VoteChangeEvent } from "../generated/schema";
+import { ensureDelegation, ensureDelegator, ensureVoter } from "./mapping";
+
+export function handleFrozenVoteChange(event: FrozenVoteChange): void {
+  const id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+  const eventObject = new VoteChangeEvent(id);
+
+  eventObject.vault = "FrozenLocking";
+
+  eventObject.amount = event.params.amount;
+  eventObject.timestamp = event.block.timestamp;
+  // Check if Voter exists
+  // create voter
+  const voter = ensureVoter(event.params.to.toHexString());
+  eventObject.to = voter.id;
+
+  // Check if Delegator exists
+  // create delegate
+  const delegator = ensureDelegator(event.params.from.toHexString());
+  eventObject.from = delegator.id;
+
+  // Check if Delegation Exists
+  // create delegation
+  const delegation = ensureDelegation(delegator, voter);
+  eventObject.delegation = delegation.id;
+  eventObject.save();
+
+  // update voting power
+  voter.votingPower = voter.votingPower.plus(event.params.amount);
+  voter.save();
+
+  // Update delegator count
+  // Is current delegation net +ve
+  const wasDelegationCounted = delegation.amount.gt(BigInt.zero());
+  // Update Delegation, check net amount
+  delegation.amount = delegation.amount.plus(event.params.amount);
+  delegation.save();
+
+  const isDelegationCounted = delegation.amount.gt(BigInt.zero());
+  // If Delegation was 0, and now +ve add one to Voter.numberOfDelegations
+  if (isDelegationCounted && !wasDelegationCounted) {
+    voter.numberOfDelegations = voter.numberOfDelegations.plus(
+      BigInt.fromI32(1)
+    );
+    voter.save();
+  }
+  // If delegation was +ve, and now 0 subtract one from Voter.numberOfDelegations
+  if (!isDelegationCounted && wasDelegationCounted) {
+    voter.numberOfDelegations = voter.numberOfDelegations.minus(
+      BigInt.fromI32(1)
+    );
+    voter.save();
+  }
+}
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,65 +1,7 @@
 import { BigInt } from "@graphprotocol/graph-ts";
-import { VoteChange } from "../generated/VotingVault/VotingVault";
-import {
-  Delegation,
-  Delegator,
-  VoteChangeEvent,
-  Voter,
-} from "../generated/schema";
+import { Delegation, Delegator, Voter } from "../generated/schema";
 
-export function handleVoteChange(event: VoteChange): void {
-  // Create a VoteChangeEvent
-  const id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
-  const eventObject = new VoteChangeEvent(id);
-
-  // Check if Voter exists
-  // create voter
-  const voter = ensureVoter(event.params.to.toHexString());
-
-  // Check if Delegator exists
-  // create delegate
-  const delegator = ensureDelegator(event.params.from.toHexString());
-
-  // Check if Delegation Exists
-  // create delegation
-  const delegation = ensureDelegation(delegator, voter);
-
-  // complete eventObject creation
-  eventObject.to = voter.id;
-  eventObject.delegation = delegation.id;
-  eventObject.amount = event.params.amount;
-  eventObject.timestamp = event.block.timestamp;
-  eventObject.save();
-
-  // update voting power
-  voter.votingPower = voter.votingPower.plus(event.params.amount);
-  voter.save();
-
-  // Update delegator count
-  // Is current delegation net +ve
-  const wasDelegationCounted = delegation.amount.gt(BigInt.zero());
-  // Update Delegation, check net amount
-  delegation.amount = delegation.amount.plus(event.params.amount);
-  delegation.save();
-
-  const isDelegationCounted = delegation.amount.gt(BigInt.zero());
-  // If Delegation was 0, and now +ve add one to Voter.numberOfDelegations
-  if (isDelegationCounted && !wasDelegationCounted) {
-    voter.numberOfDelegations = voter.numberOfDelegations.plus(
-      BigInt.fromI32(1)
-    );
-    voter.save();
-  }
-  // If delegation was +ve, and now 0 subtract one from Voter.numberOfDelegations
-  if (!isDelegationCounted && wasDelegationCounted) {
-    voter.numberOfDelegations = voter.numberOfDelegations.minus(
-      BigInt.fromI32(1)
-    );
-    voter.save();
-  }
-}
-
-function ensureVoter(address: string): Voter {
+export function ensureVoter(address: string): Voter {
   let voter = Voter.load(address);
 
   if (!voter) {
@@ -70,7 +12,7 @@ function ensureVoter(address: string): Voter {
   return voter;
 }
 
-function ensureDelegator(address: string): Delegator {
+export function ensureDelegator(address: string): Delegator {
   let delegator = Delegator.load(address);
 
   if (!delegator) {
@@ -81,7 +23,10 @@ function ensureDelegator(address: string): Delegator {
   return delegator;
 }
 
-function ensureDelegation(delegator: Delegator, voter: Voter): Delegation {
+export function ensureDelegation(
+  delegator: Delegator,
+  voter: Voter
+): Delegation {
   let id = delegator.id + "-" + voter.id;
 
   let delegation = Delegation.load(id);
